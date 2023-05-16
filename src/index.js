ff4662050a02e4ad83d4b4e1eb33603a1c5c3dd6
mosidrum <mosiokanga@gmail.com>
2023-05-16 17:15:57 +0100

    Reconfigured webpack

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import _ from 'lodash';
-import printMe from './print.js';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-   return element;
- }
-
- document.body.appendChild(component());
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    return element;
+  }
+  
+  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: './src/index.js',
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
-  // optimization: {
-  //   runtimeChunk: 'single',
-  // },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-   clean: true,
-   publicPath: '/',
   },
+  plugins: [
+
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+    }),
+
+  ],
   module: {
+
     rules: [
+
       {
+
         test: /\.css$/i,
+
         use: ['style-loader', 'css-loader'],
+
       },
+
     ],
+
   },
 };
