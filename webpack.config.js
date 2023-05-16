64f6abaa337f20ec354f6db09d2efb465f5097fa
mosidrum <mosiokanga@gmail.com>
2023-05-16 17:23:04 +0100

    Reconfigured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
